4e0196fbc132c22ae30be9787fea39c6f37fe11c
viys <2628587386@qq.com>
2024-09-13 15:06:31 +0800

    使用了一些语法

--- project/app/main.c
+++ project/app/main.c
@@ -3,12 +3,7 @@
 int main()
 {
     printf("BSL\n");
-#ifdef CONFIG_MY_DEBUG
-    printf("启用调试功能\n");
-#endif
-#ifdef CONFIG_COLOR_LOG_MODE
-    printf("开启日志颜色\n");
-#endif
+    printf("固件版本号: %s\n", CONFIG_FIRMWARE_VERSION);
 
 #ifdef CONFIG_ENABLE_SLEEP
     printf("启用休眠功能\n");
@@ -18,17 +13,27 @@ int main()
     printf("启用身份验证\n");
 #endif
 
+#ifdef CONFIG_DEBUG
+    printf("启用调试功能\n");
+#endif
+#ifdef CONFIG_DEBUG_COLOR_MODE
+    printf("    开启日志颜色\n");
+#endif
+#ifdef CONFIG_DEBUG_LEVEL
+    printf("    日志等级 LEVEL:%d\n", CONFIG_DEBUG_LEVEL);
+#endif
+
+
 #ifdef CONFIG_ENABLE_USB_CDC
     printf("启用 USB CDC 功能\n");
 #endif
-
 #ifdef CONFIG_ENABLE_USB_CDC_DBG
-    printf("启用 USB 日志\n");
+    printf("    启用 USB 日志\n");
 #endif
 
 #ifdef CONFIG_ENABLE_PROACTIVE_NOTIFY
     printf("启用蓝牙消息主动上报功能\n");
 #endif
-	printf("固件版本号: %s\n", CONFIG_FIRMWARE_VERSION);
+	
     return 0;
 }
